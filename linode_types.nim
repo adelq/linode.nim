7dcfcde9a23b6365bd7c4f6c0fe1df7a208306cd
Adel Qalieh <aqalieh95@gmail.com>
2015-05-16 23:52:25 -0400

    Add Linode type to work with later

---
+++ linode_types.nim
@@ -0,0 +1,28 @@
+import times
+
+type Linode* = object
+  TotalXFer: int
+  BackupsEnabled: int
+  WatchDog: int
+  LpmDisplayGroup: string
+  AlertBwQuotaEnabled: int
+  Status: int
+  TotalRAM: int
+  AlertDiskIOThreshold: int
+  BackupWindow: int
+  AlertBwOutEnabled: int
+  AlertBwOutThreshold: int
+  Label: string
+  AlertCPUEnabled: int
+  AlertBwQuotaThreshold: int
+  AlertBwInThreshold: int
+  BackupWeeklyDay: int
+  DataCenterId: int
+  AlertCPUThreshold: int
+  TotalHD: int
+  AlertDiskIOEnabled: int
+  AlertBwInEnabled: int
+  LinodeId: int
+  CreateDt: Time
+  PlanId: int
+  DistributionVendor: string
