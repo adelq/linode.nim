b8866402a7ac0d6d3730b2dc7fac519e97ac8066
Adel Qalieh <aqalieh95@gmail.com>
2015-05-16 23:52:55 -0400

    Make methods public plus rework of ABI

--- linode.nim
+++ linode.nim
@@ -3,14 +3,16 @@ import tables
 
 const BASE_URL = "https://api.linode.com/api/"
 
-type Client = tuple[url: string, api_key: string]
+type Client* = tuple[url: string, api_key: string]
 
-proc request(client: Client, params:Table[string, string]) =
+proc request(client: Client, params:Table[string, string]): string =
   var body: string
   let http_method = "GET"
+  let url = BASE_URL
+  return getContent(url)
 
-proc action(client: Client, action: string, params:Table[string, string]) =
+proc action*(client: Client, action: string, params:Table[string, string]): string =
   var params = params
   params.add("api_key", client.api_key)
   params.add("api_action", action)
-  client.request(params)
+  return client.request(params)
